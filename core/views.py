a5b8b879ee36d2a809c93bb7cbbf9b179dbb180f
Diana Karen Parra Sosa <dianaparra092@gmail.com>
2020-05-19 22:41:30 -0500

    SSubida de rtemplates

--- core/views.py
+++ core/views.py
@@ -1,36 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/portfolio/">Portafolio</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-</ul>
-"""
-
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + """
-        <h2>Prtada</h2>
-        <p>Esto es la portada</p>
-    """)
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(html_base + """
-    <h2>Acerca de</h2>
-    <p>Me llamo Diana y soy Diseñadora.</p>
-    """)
+    return render(request, "core/about.html")
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-    <h2>Portafolio</h2>
-    <p>Esto es el portafolio</p>
-    """)
+    return render(request, "core/portfolio.html")
 
 def contact(request):
-    return HttpResponse(html_base + """
-    <h2>Contacto</h2>
-    <p>Esta el la información de contacto</p>
-    """)
+    return render(request, "core/contact.html")
